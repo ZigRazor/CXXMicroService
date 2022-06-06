696bc0579f7859f3c4d48c25c9e2d3e80c74bda2
ZigRazor <zigrazor@gmail.com>
2022-06-06 12:38:42 +0200

    Removed Redundant Blank line

--- src/ZMQMicroServiceClient.cpp
+++ src/ZMQMicroServiceClient.cpp
@@ -31,7 +31,6 @@ namespace MicroServices
         socket = new zmq::socket_t(ctx, zmq::socket_type::client);
 
         MICROSERVICE_LOG(MICROSERVICE_LOG_DEBUG, logger, "Created ZMQMicroServiceClient " << serviceConfiguration.serviceName);
-
     }
 
     ZMQMicroServiceClient::~ZMQMicroServiceClient()
